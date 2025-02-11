13b87ca5aeeea5f54af2c8b71d600169538341bf
hieule88 <hieu.le@moreh.com.vn>
2025-02-11 17:42:24 +0700

    add kernel

--- src/kernels/MIOpenTypeCast.cpp
+++ src/kernels/MIOpenTypeCast.cpp
@@ -49,9 +49,9 @@ __device__ void typeCast(const TI* __restrict__ input,
     TI val = input[input_tv.get_tensor_view_idx(layout)];
     if(bits_to_truncate)
     {
-        val = truncate_func(gid, val, bits_to_truncate);
+        val = truncate_func<TI>(val, bits_to_truncate);
     }
-    output[output_tv.get_tensor_view_idx(layout)] = cast_func(val);
+    output[output_tv.get_tensor_view_idx(layout)] = cast_func<TI, TO>(val);
 }
 
 extern "C" __global__ void TypeCast(const I_TYPE* __restrict__ input,
@@ -79,9 +79,9 @@ __device__ void typeCastContiguous(const TI* __restrict__ input,
     TI val = input[gid];
     if(bits_to_truncate)
     {
-        val = truncate_func(gid, val, bits_to_truncate);
+        val = truncate_func<TI>(val, bits_to_truncate);
     }
-    output[gid] = cast_func(val);
+    output[gid] = cast_func<TI, TO>(val);
 }
 
 extern "C" __global__ void TypeCastContiguous(const I_TYPE* __restrict__ input,
@@ -92,101 +92,32 @@ extern "C" __global__ void TypeCastContiguous(const I_TYPE* __restrict__ input,
     typeCastContiguous<I_TYPE, O_TYPE>(input, output, bits_to_truncate, numel);
 }
 
-inline DTYPE truncate_func(size_t gid, DTYPE val, int nbits)
+template <typename DTYPE>
+inline DTYPE truncate_func(DTYPE val, uint64_t nbits)
 {
-#if MIOPEN_USE_FP32 == 1
-    return as_float((as_uint(val) & (static_cast<uint>(0xFFFFFFFF) << nbits)));
-#elif MIOPEN_USE_FP64 == 1
-    return as_double((as_ulong(val) & ((ulong)0xFFFFFFFFFFFFFFFF << nbits)));
+#if IS_I_TYPE_FP32 == 1
+    return __uint_as_float((__float_as_uint(val) & (static_cast<uint>(0xFFFFFFFF) << nbits)));
+#elif IS_I_TYPE_FP64 == 1
+    return __longlong_as_double(
+        (__double_as_longlong(val) & (static_cast<long long>(0xFFFFFFFFFFFFFFFF) << nbits)));
 #else
     return val;
 #endif
 }
 
-template <typename T>
-__device__ bool convert_bool(T x)
+template <typename TI, typename TO>
+inline TO cast_func(TI val)
 {
-    return static_cast<bool>(x);
-}
-
-template <typename T>
-__device__ ushort convert_bfloat16(T x)
-{
-#if MIOPEN_USE_FP32 == 1
-    return (as_uint(x) + ((as_uint(x) >> 16) & 1) + (static_cast<uint>(0x7FFF))) >> 16;
+#if IS_O_TYPE_BFP16 == 1
+#if IS_I_TYPE_FP32
+    return (__float_as_uint(x) + ((__float_as_uint(x) >> 16) & 1) + (static_cast<uint>(0x7FFF))) >>
+           16;
 #else
-    return (as_uint(convert_float(x)) + ((as_uint(convert_float(x)) >> 16) & 1) +
-            (static_cast<uint>(0x7FFF))) >>
+    return (__float_as_uint(static_cast<float>(x)) +
+            ((__float_as_uint(static_cast<float>(x)) >> 16) & 1) + (static_cast<uint>(0x7FFF))) >>
            16;
 #endif
+#else
+    return static_cast<TO>(val);
+#endif
 }
-
-template <typename T>
-__device__ half2 convert_complex32(T x)
-{
-    return (half2)(convert_half(x), (half)0);
-}
-
-template <typename T>
-__device__ float2 convert_complex64(T x)
-{
-    return (float2)(convert_float(x), (float)0);
-}
-
-template <typename T>
-__device__ double2 convert_complex128(T x)
-{
-    return (double2)(convert_double(x), (double)0);
-}
-
-#define TYPECAST(dest_type, dest_type_ptr, cast_func)                                             \
-    extern "C" __global__ void Typecast_##dest_type(const I_TYPE* __restrict__ input,             \
-                                                    dest_type_ptr __restrict__ output,            \
-                                                    uint64_t numel,                               \
-                                                    uint64_t bits_to_truncate)                    \
-    {                                                                                             \
-        typeCast<I_TYPE, dest_type>(input, output, bits_to_truncate, numel, input_tv, output_tv); \
-    }
-
-#define TYPECASTCONTIGUOUS(dest_type, dest_type_ptr, cast_func)                                  \
-    extern "C" __global__ void TypecastContiguous_##dest_type(const I_TYPE* __restrict__ input,  \
-                                                              dest_type_ptr __restrict__ output, \
-                                                              uint64_t numel,                    \
-                                                              uint64_t bits_to_truncate)         \
-    {                                                                                            \
-        typeCastContiguous<I_TYPE, dest_type>(input, output, bits_to_truncate, numel);           \
-    }
-
-TYPECAST(bool, bool*, convert_bool);
-TYPECAST(char, char*, convert_char);
-TYPECAST(short, short*, convert_short);
-TYPECAST(int, int*, convert_int);
-TYPECAST(long, long*, convert_long);
-TYPECAST(uchar, uchar*, convert_uchar);
-TYPECAST(ushort, ushort*, convert_ushort);
-TYPECAST(uint, uint*, convert_uint);
-TYPECAST(ulong, ulong*, convert_ulong);
-TYPECAST(half, half*, convert_half);
-TYPECAST(float, float*, convert_float);
-TYPECAST(double, double*, convert_double);
-TYPECAST(bfloat16, ushort*, convert_bfloat16);
-TYPECAST(complex32, half2*, convert_complex32);
-TYPECAST(complex64, float2*, convert_complex64);
-TYPECAST(complex128, double2*, convert_complex128);
-
-TYPECASTCONTIGUOUS(bool, bool*, convert_bool);
-TYPECASTCONTIGUOUS(char, char*, convert_char);
-TYPECASTCONTIGUOUS(short, short*, convert_short);
-TYPECASTCONTIGUOUS(int, int*, convert_int);
-TYPECASTCONTIGUOUS(long, long*, convert_long);
-TYPECASTCONTIGUOUS(uchar, uchar*, convert_uchar);
-TYPECASTCONTIGUOUS(ushort, ushort*, convert_ushort);
-TYPECASTCONTIGUOUS(uint, uint*, convert_uint);
-TYPECASTCONTIGUOUS(ulong, ulong*, convert_ulong);
-TYPECASTCONTIGUOUS(half, half*, convert_half);
-TYPECASTCONTIGUOUS(float, float*, convert_float);
-TYPECASTCONTIGUOUS(double, double*, convert_double);
-TYPECASTCONTIGUOUS(bfloat16, ushort*, convert_bfloat16);
-TYPECASTCONTIGUOUS(complex32, half2*, convert_complex32);
-TYPECASTCONTIGUOUS(complex64, float2*, convert_complex64);
-TYPECASTCONTIGUOUS(complex128, double2*, convert_complex128);
